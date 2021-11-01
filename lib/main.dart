aa69da5f4ca7910f2fb518933bbd0821cef415d3
Aby T Mathew <abymathewtkv@gmail.com>
2021-11-01 16:38:05 +0530

    Initially updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,68 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bill_payment_app/screen_two.dart';
 
-import 'my_home_page.dart';
+void main() => runApp(MyApp());
 
-
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.red,
-        
       ),
-      home:MyHomePage(),
-      
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        body: HomeApp(),
+      ),
     );
-   
+  }
+}
+
+class HomeApp extends StatefulWidget {
+  _HomeAppState createState() => _HomeAppState();
+}
+
+class _HomeAppState extends State<HomeApp> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(
+        Duration(seconds: 5),
+        () => Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => MyAppScreenTwo())));
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Align(
+          alignment: Alignment.center,
+          child: Image.asset(
+            'assets/images/pic.jpg',
+            width: 200,
+            height: 150,
+          ),
+        ),
+        SizedBox(
+          height: 25,
+        ),
+        Align(
+          alignment: Alignment.center,
+          child: Text(
+            "QPay",
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: (FontWeight.bold),
+                color: Colors.white),
+          ),
+        ),
+      ],
+    );
+  }
 }
