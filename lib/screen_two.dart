724c04737e5ad99c0d8324ff945d6f601cb7389e
Aby T Mathew <abymathewtkv@gmail.com>
2021-11-02 17:16:00 +0530

    updated

--- lib/screen_two.dart
+++ lib/screen_two.dart
@@ -7,20 +7,35 @@ class MyAppScreenTwo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return  MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: "Registration ",
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        primaryColor: Color(0xFFC41A38),
+        primaryColorLight: Color(0xFFFBE0E6),
+        accentColor: Color(0xFF1B1F32),
+
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Screen Two'),
-          centerTitle: true,
-        ),
-        body: const Center(
-          child: Text('Welcome to second page'),
-        ),
-      ),
+      home: MyHomePage(),
+
+    );
+     
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  // ignore: no_logic_in_create_state
+  _MyHomePageState createState() => _MyHomePageState(); 
+}
+
+class _MyHomePageState extends State<MyHomePage>{
+  String title = 'SignUp';
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+
     );
   }
+
 }
