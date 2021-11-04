b6cc796bbd387a3fe42ed175f7a33f50fbe4ef4f
Aby T Mathew <abymathewtkv@gmail.com>
2021-11-04 17:17:08 +0530

    container has been created

--- lib/screen_two.dart
+++ lib/screen_two.dart
@@ -30,10 +30,35 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage>{
-  String title = 'SignUp';
+  
   @override
   Widget build(BuildContext context){
     return Scaffold(
+      body: Container(
+        color: Color(0xFFC41A38),
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
+        
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 75.0,
+            ),
+            Container(
+              height: 75.0,
+              width: 75.0,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/pic2.png')
+                )
+              ),
+            ),
+            Text('SignUp',style: TextStyle(fontSize: 28.0),),
+            SizedBox(height: 8.0,),
+            Text('Welcome',style: TextStyle(fontSize: 14.0),)
+          ],
+        ),
+      ),
 
     );
   }
