d1180e8a3774d5d7c161a5c6ec7a426458695b60
Aby T Mathew <abymathewtkv@gmail.com>
2021-11-05 12:56:04 +0530

    TextField is added

--- lib/screen_two.dart
+++ lib/screen_two.dart
@@ -30,59 +30,90 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ConstrainedBox(
-        constraints:
-            BoxConstraints(maxHeight: MediaQuery.of(context).size.height),
-        child: Container(
-          color: Color(0xFFC41A38),
-          height: MediaQuery.of(context).size.height,
-          width: double.infinity,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              SizedBox(
-                height: 10.0,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                    top: 0.0, right: 0.0, left: 0.0, bottom: 0.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Container(
-                      height: 70.0,
-                      width: 70.0,
-                      decoration: BoxDecoration(
-                          image: DecorationImage(
-                              image: AssetImage('assets/images/pic2.png'))),
+      body: SafeArea(
+        child: ConstrainedBox(
+          constraints:
+              BoxConstraints(maxHeight: MediaQuery.of(context).size.height),
+          child: Container(
+            color: Color(0xFFC41A38),
+            height: MediaQuery.of(context).size.height,
+            width: double.infinity,
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+              
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        top: 0.0, right: 0.0, left: 0.0, bottom: 0.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          height: 70.0,
+                          width: 70.0,
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage('assets/images/pic2.png'),fit: BoxFit.cover)),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  Text(
+                    'SignUp',
+                    style: TextStyle(
+                      fontSize: 28.0,
+                      color: Color(0xFFFBE0E6),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  Text(
+                    'Welcome',
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      color: Color(0xFFFBE0E6),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Color(0xFFFBE0E6),
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(15.0),
+                            topRight: Radius.circular(15.0))),
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 10.0,left: 10.0,right: 10.0,bottom: 0.0),
+                      child: Container(
+                      
+                        height: MediaQuery.of(context).size.height / 1.5,
+                        width: MediaQuery.of(context).size.width,
+                        child: Container(
+                          height: 40.0,
+                          width: MediaQuery.of(context).size.width / 1.2,
+                          decoration: BoxDecoration(
+                            color: Color(0xFFC41A3B)
+                          ),
+                          child: TextField(
+                             cursorColor: Color(0xFFFBE0E6),
+                            decoration: InputDecoration(
+                              hintText: "Name",hintStyle: TextStyle(color: Color(0xFFFBE0E6))
+                            ),
+                           
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              Text(
-                'SignUp',
-                style: TextStyle(
-                  fontSize: 28.0,
-                  color: Color(0xFFFBE0E6),
-                ),
-              ),
-              SizedBox(
-                height: 8.0,
-              ),
-              Text(
-                'Welcome',
-                style: TextStyle(
-                  fontSize: 14.0,
-                  color: Color(0xFFFBE0E6),
-                ),
-              ),
-              Container(
-                color: Colors.white,
-                height: MediaQuery.of(context).size.height/1.5,
-                width: MediaQuery.of(context).size.width,
-             
-              ),
-            ],
+            ),
           ),
         ),
       ),
