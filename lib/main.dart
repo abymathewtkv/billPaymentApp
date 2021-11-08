5dc8f2ddb1e694fac6d6acbde13f1fbb7d05849d
Aby T Mathew <abymathewtkv@gmail.com>
2021-11-08 16:58:09 +0530

    page updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_bill_payment_app/screen_two.dart';
+
+import 'package:flutter_bill_payment_app/splash_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -19,42 +18,6 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.white,
         body: HomeApp(),
       ),
-    );
-  }
-}
-
-class HomeApp extends StatefulWidget {
-  const HomeApp({Key? key}) : super(key: key);
-
-  @override
-  _HomeAppState createState() => _HomeAppState();
-}
-
-class _HomeAppState extends State<HomeApp> {
-  @override
-  void initState() {
-    super.initState();
-
-    Timer(
-        const Duration(seconds: 5),
-        () => Navigator.pushReplacement(context,
-            MaterialPageRoute(builder: (context) => const MyAppScreenTwo())));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Align(
-          alignment: Alignment.center,
-          child: Image.asset(
-            'assets/images/logo.jpg',
-            width: 300,
-            height: 300,
-          ),
-        ),
-      ],
     );
   }
 }
