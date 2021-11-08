925d4f21e1c0322d6fa43b5a08affbe0474f8c28
Aby T Mathew <abymathewtkv@gmail.com>
2021-11-08 20:44:21 +0530

    text size is changed

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,7 +1,8 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_bill_payment_app/screen_two.dart';
+
+import 'ScreenTwo/screen_two.dart';
 
 class HomeApp extends StatefulWidget {
   const HomeApp({Key? key}) : super(key: key);
@@ -26,35 +27,33 @@ class _HomeAppState extends State<HomeApp> {
     return Stack(
       children: <Widget>[
         Container(
-          color: Color(0xFF03A9F4),
+          color: const Color(0xFF03A9F4),
         ),
         Center(
-          child: Container(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                CircleAvatar(
-                  backgroundColor: Colors.white,
-                  radius: 85,
-                  child: Image(
-                    image: AssetImage('assets/images/logo.jpg'),
-                    height: 110.0,
-                    width: 110.0,
-                    fit: BoxFit.cover,
-                  ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const <Widget>[
+              CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: 85,
+                child: Image(
+                  image: AssetImage('assets/images/logo.jpg'),
+                  height: 110.0,
+                  width: 110.0,
+                  fit: BoxFit.cover,
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Text(
-                    'QuickPAY',
-                    style: TextStyle(
-                        fontSize: 24.0,
-                        color: Colors.white,
-                        fontWeight: (FontWeight.bold)),
-                  ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text(
+                  'QuickPAY',
+                  style: TextStyle(
+                      fontSize: 32.0,
+                      color: Colors.white,
+                      fontWeight: (FontWeight.bold)),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ],
